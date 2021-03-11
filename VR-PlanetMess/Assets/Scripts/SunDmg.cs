2aaf1f6ea35a5cf11f0253a6d3d55d7565e6c7e0
Madeline <bertrandemilien@gmail.com>
2021-03-11 15:02:40 +0100

    Y'a ptetre de ça

---
+++ VR-PlanetMess/Assets/Scripts/SunDmg.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunDmg : MonoBehaviour
+{
+    public int hp = 3;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Meteor"))
+        {
+            hp--;
+            Debug.Log(hp);
+            if(hp == 0)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
