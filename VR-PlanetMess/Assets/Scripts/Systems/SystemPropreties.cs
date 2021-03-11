685f49a4fea071cc8534f9d833799f25571784df
Vincent <vincentpy0902@gmail.com>
2021-03-11 15:31:17 +0100

    Système solaire modifié

--- VR-PlanetMess/Assets/Scripts/Systems/SystemPropreties.cs
+++ VR-PlanetMess/Assets/Scripts/Systems/SystemPropreties.cs
@@ -6,27 +6,73 @@ public class SystemPropreties : MonoBehaviour
 {
     public GameObject sunPrefab;
     public SunData sunData;
-    [Header("0, petit, 1 moyen, 2 grand")]
+    [Tooltip("0, petit, 1 moyen, 2 grand")]
     [Range(0,2)]
     public int sunSize;
 
-    [Space(50)]
+    private Transform[] orbit = new Transform[3];
+
+    [Space(30)]
     public GameObject planetPrefab;
     public PlanetData planetData;
-    [Header("0, petit, 1 moyen, 2 grand")]
+
+    [Space(10)]
+    [Header("Planète 1 = petit orbite, Planète 2 = moyen orbite, Planète 3 = grand orbite")]
+    [Tooltip("0, petit, 1 moyen, 2 grand")]
     [Range(0, 2)]
-    public int planetSize;
-    [Header("0, froid, 1 tempéré, 2 chaud")]
+    public int planet1Size;
+    [Tooltip("0, froid, 1 tempéré, 2 chaud")]
     [Range(0, 2)]
-    public int planetTemp;
-    [Header("0, gazeux, 1 rocheux, 2 aquatique")]
+    public int planet1Temp;
+    [Tooltip("0, gazeux, 1 rocheux, 2 aquatique")]
     [Range(0, 2)]
-    public int planetCompo;
+    public int planet1Compo;
+    [Tooltip("0, petit, 1 moyen, 2 grand")]
+
+    [Space(10)]
+    [Range(0, 2)]
+    public int planet2Size;
+    [Tooltip("0, froid, 1 tempéré, 2 chaud")]
+    [Range(0, 2)]
+    public int planet2Temp;
+    [Tooltip("0, gazeux, 1 rocheux, 2 aquatique")]
+    [Range(0, 2)]
+    public int planet2Compo;
+    [Tooltip("0, petit, 1 moyen, 2 grand")]
+
+    [Space(10)]
+    [Range(0, 2)]
+    public int planet3Size;
+    [Tooltip("0, froid, 1 tempéré, 2 chaud")]
+    [Range(0, 2)]
+    public int planet3Temp;
+    [Tooltip("0, gazeux, 1 rocheux, 2 aquatique")]
+    [Range(0, 2)]
+    public int planet3Compo;
 
     private void Start()
     {
-        GameObject sunPrefabClone = Instantiate(sunPrefab, transform.position, Quaternion.identity, transform);
-        sunPrefabClone.transform.localScale = new Vector3(sunData.size[sunSize], sunData.size[sunSize], sunData.size[sunSize]);
+        //Soleil
+        GameObject sun = Instantiate(sunPrefab, transform.position, Quaternion.identity, transform);
+        sun.transform.localScale = new Vector3(sunData.size[sunSize], sunData.size[sunSize], sunData.size[sunSize]);
+
+        //Orbit
+        orbit[0] = transform.Find("Orbits/Small Orbit");
+        orbit[1] = transform.Find("Orbits/Medium Orbit");
+        orbit[2] = transform.Find("Orbits/Big Orbit");
+
+        //Planets
+        GameObject planet1 = Instantiate(planetPrefab, orbit[0].position, Quaternion.identity, orbit[0]);
+        planet1.transform.localScale = new Vector3(planetData.size[planet1Size], planetData.size[planet1Size], planetData.size[planet1Size]);
+        planet1.transform.GetComponent<MeshRenderer>().material = planetData.temp[planet1Temp];
+
+        GameObject planet2 = Instantiate(planetPrefab, orbit[1].position, Quaternion.identity, orbit[1]);
+        planet2.transform.localScale = new Vector3(planetData.size[planet2Size], planetData.size[planet2Size], planetData.size[planet2Size]);
+        planet2.transform.GetComponent<MeshRenderer>().material = planetData.temp[planet2Temp];
+
+        GameObject planet3 = Instantiate(planetPrefab, orbit[2].position, Quaternion.identity, orbit[2]);
+        planet3.transform.localScale = new Vector3(planetData.size[planet3Size], planetData.size[planet3Size], planetData.size[planet3Size]);
+        planet3.transform.GetComponent<MeshRenderer>().material = planetData.temp[planet3Temp];
     }
 
     private void Update()
@@ -114,16 +160,4 @@ public class SystemPropreties : MonoBehaviour
         }*/
         #endregion
     }
-
-
-    /*private void FindSystemsTransforms()
-    {
-        GameObject[] systemSpawnPoints = GameObject.FindGameObjectsWithTag("SystemSpawnPoint");
-        for (int i = 0; i < systemSpawnPointTransform.Length; i++)
-        {
-            systemSpawnPointTransform[i] = systemSpawnPoints[i].transform;
-        }
-    }*/
-
-
 }
