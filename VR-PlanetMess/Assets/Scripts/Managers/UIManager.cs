f64168c19b71396aaf6d106f8b3943844d9e2444
ErbakAsrevnel <jeanbaptistemaupin@gmail.com>
2021-03-11 14:14:05 +0100

    Ajout d'une scène personnelle + modifications dans l'UIManager

--- VR-PlanetMess/Assets/Scripts/Managers/UIManager.cs
+++ VR-PlanetMess/Assets/Scripts/Managers/UIManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject OptionsMenuUI;
+    public GameObject MainMenuUI;
+    
+    private void Start() 
     {
-        
+        OptionsMenuUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+
+    }
+    public void StartToPlay()
+    {
+        //LoadScenePlay
+        Debug.Log("I play the game!");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OptionsMenu()
     {
-        
+        Debug.Log("AAAAAAAAAH TURN THE VOLUME DOOOOOWN");
+        OptionsMenuUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+        //OptionsScreen actif
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("So long hey Bowser");
+        Application.Quit();    
+    }
+    public void BackToMainMenu()
+    {
+        Debug.Log("Welcome back, ears better?");
+        OptionsMenuUI.SetActive(false);
+        MainMenuUI.SetActive(true);
     }
 }
