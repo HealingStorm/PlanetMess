0361cf27f8dacf8c039b050e2fb86981f765b34e
Madeline <bertrandemilien@gmail.com>
2021-03-11 14:42:08 +0100

    Heu yo

--- VR-PlanetMess/Assets/Scripts/MeteorSpawn.cs
+++ VR-PlanetMess/Assets/Scripts/MeteorSpawn.cs
@@ -39,10 +39,11 @@ public class MeteorSpawn : MonoBehaviour
         yield return new WaitForSeconds(timer);
 
         Debug.Log("Timer = " + timer);
-        suns = GameObject.FindGameObjectsWithTag("Sun");
+        spawnPoints = GameObject.FindGameObjectsWithTag("Spawner");
         randomSpawnPoint = UnityEngine.Random.Range(0, 19);
         GameObject actualMeteor = Instantiate(meteor, spawnPoints[randomSpawnPoint].transform.position, spawnPoints[randomSpawnPoint].transform.rotation);
         randomSun = UnityEngine.Random.Range(0, 3);
+        suns = GameObject.FindGameObjectsWithTag("Sun");
         actualMeteor.GetComponent<MeteorBehaviour>().sunPosition = suns[randomSun];
         random = UnityEngine.Random.Range(minTimeToSpawn, maxTimeToSpawn);
 
