0276e1d163ae91c06697775be6516cbf0d6a9e8f
ErbakAsrevnel <jeanbaptistemaupin@gmail.com>
2021-03-25 14:29:49 +0100

    Ajout d'un menu pause et d'un menu de fin de niveau (non fonctionnel)

--- VR-PlanetMess/Assets/Scripts/Managers/UIManager.cs
+++ VR-PlanetMess/Assets/Scripts/Managers/UIManager.cs
@@ -9,6 +9,8 @@ public class UIManager : MonoBehaviour
     private GameObject OptionsMenuUI;
     private GameObject MainMenuUI;
     private GameObject LevelSelectMenuUI;
+    private GameObject PauseMenuUI;
+    private GameObject LevelCompleteMenuUI;
     #region Singlton:Profile
 
     public static UIManager Instance;
@@ -78,4 +80,23 @@ public class UIManager : MonoBehaviour
         Debug.Log("Tu fais le tuto mais en fait c'est le jeu");
         SceneManager.LoadScene("Level 1");
     }
+
+    public void ReturnToMainMenu()
+    {
+        Debug.Log("Bah alors on ragequit?");
+        PauseMenuUI.SetActive(false);
+        LevelCompleteMenuUI.SetActive(false);
+        LevelSelectMenuUI.SetActive(false);
+        OptionsMenuUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Bon la pause est finie on reprend");
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
 }
