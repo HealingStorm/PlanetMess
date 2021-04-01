1b4f330d8f2ad401b9fae5ea84823fe395a6082b
Madeline <bertrandemilien@gmail.com>
2021-04-01 15:27:04 +0200

    Samuel t'es un boulle et bil

--- VR-PlanetMess/Assets/Scripts/Sun/SunDmg.cs
+++ VR-PlanetMess/Assets/Scripts/Sun/SunDmg.cs
@@ -6,6 +6,10 @@ public class SunDmg : MonoBehaviour
 {
     public int hp = 3;
 
+    public GameObject playerDeath;
+
+    public GameObject deathVfx;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Meteor"))
@@ -15,6 +19,12 @@ public class SunDmg : MonoBehaviour
             if(hp == 0)
             {
                 FindObjectOfType<AudioManager>().Play("Game Over");
+                GameObject playerMort = Instantiate(playerDeath, GameObject.FindGameObjectWithTag("Player").transform.position, GameObject.FindGameObjectWithTag("Player").transform.rotation);
+                playerMort.transform.parent = null;
+                Destroy(GameObject.FindGameObjectWithTag("Player"));
+                GameObject explosion = Instantiate(deathVfx, transform.position, transform.rotation);
+                Destroy(explosion, 7f);
+                explosion.transform.parent = null;
                 Destroy(this.gameObject);
             }
             else
